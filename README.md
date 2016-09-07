2d18cbf19ab647a7a961aefafc2ba967b9ec6b48
Fred Qin <fredqin87@gmail.com>
2016-09-06 17:07:25 -0700

    add todo list in readme.md

---
+++ README.md
@@ -0,0 +1,10 @@
+## BC house
+### deployment on openshift
+Enable //url: config.MONGODB_DB_URL + config.APP_NAME, in app.js line 91
+
+### todo list
+- run web scrapping every day 
+- run the request at specific time
+- save the data to mongodb server
+- create front end form
+- searching should be from local database
--- controllers/house.js
+++ controllers/house.js
@@ -8,7 +8,7 @@ var cheerio = require('cheerio');
  * Login page.
  */
 exports.getHouseListing = function (req, res) {
-    const url = 'http://www.realtylink.org/prop_search/Summary.cfm?BCD=GV&areatitle=Vancouver%20West&ARPK=37,44,36,26,10105,41,21,32,30,28,23,33,22,39,24,43,29,40,34,853,31,35,42,27&imdp=&RSPP=5&AIDL=21&SRTB=P_Price&ERTA=false&MNAGE=0&MXAGE=200&MNBD=1&MNBT=1&PTYTID=5&MNPRC=200000&MXPRC=1000000&SCTP=RA'
+    const url = 'http://www.realtylink.org/prop_search/Summary.cfm?BCD=GV&imdp=11&RSPP=5&AIDL=248%2C249%2C250%2C251%2C253%2C254%2C255%2C256%2C257%2C258%2C259%2C260%2C261%2C856%2C857%2C858%2C262%2C263%2C264%2C265%2C266%2C267%2C268%2C269%2C270%2C271%2C272%2C273%2C274%2C275%2C276%2C277%2C278%2C279%2C280%2C281%2C282&SRTB=P_Price&ERTA=True&MNAGE=0&MXAGE=200&MNBT=0&MNBD=0&PTYTID=2&MNPRC=200000&MXPRC=900000&SCTP=RS&ARPK=&AreaTitle=&agentid=&ComID=&rowp=46&vse.x=3&vse.y=4'
 
     request(url, function(error, response, html){
         res.send(html)
