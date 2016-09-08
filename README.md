654875b751ce76d94362586baf13cd32f04275ab
Fred Qin <fredqin87@gmail.com>
2016-09-07 23:39:25 -0700

    get sub area id

--- README.md
+++ README.md
@@ -4,7 +4,6 @@ Enable //url: config.MONGODB_DB_URL + config.APP_NAME, in app.js line 91
 
 ### todo list
 - update house list by mls number
-- 
 - run web scrapping every day 
 - page scrapping should be 10 seconds at a time
 - run the request at specific time
--- controllers/house.js
+++ controllers/house.js
@@ -4,26 +4,31 @@ var request = require('request');
 var cheerio = require('cheerio');
 
 /**
- * GET /api/house
+ * Private function to update house listing in mongodb server
+ *
+ * POST /api/house
  * Login page.
  */
 exports.getHouseListing = function (req, res) {
 
     // 20 is the number of listing you can view per time
-    const vancouver_area_listing_url = 'http://www.realtylink.org/prop_search/AreaSelect.cfm?AreaID=2';
+    const subarea_selection_url = 'http://www.realtylink.org/prop_search/AreaSelect.cfm?AreaID=' + req.body.AreaID;
 
     // find vancouver list
-    request(vancouver_area_listing_url, function (error, response, html) {
+    request(subarea_selection_url, function (error, response, html) {
         if (!error) {
             var $ = cheerio.load(html);
 
             // find selection
             var options = $('select[name="AreaID"]').children();
+
+            var subAreaIDList = [];
             options.filter(function () {
                 var data = $(this);
-                console.log(data.html())
-
+                var subAreaID = data.attr('value');
+                subAreaIDList.push(subAreaID);
             });
+            console.log(subAreaIDList)
         }
     })
 };
